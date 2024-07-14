c7881b928bff5d994feccd2ec95467ef1fa380d8
giovanesd <giovanesd@gmail.com>
2024-07-14 16:35:47 -0300

    adding incrementer to the job.

--- src/main/java/com/example/demo/Springbatch5courseConfig.java
+++ src/main/java/com/example/demo/Springbatch5courseConfig.java
@@ -3,11 +3,15 @@ package com.example.demo;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.PlatformTransactionManager;
@@ -19,16 +23,23 @@ public class Springbatch5courseConfig {
 	public Job imprimeOlaMundoJob(JobRepository jobRepository, Step step) {
 		return new JobBuilder("imprimeOlaMundoJob", jobRepository)
 				.start(step)
+				.incrementer(new RunIdIncrementer())
 				.build();
 	}
 
 	@Bean
 	public Step imprimeOlaMundoStep(JobRepository jobRepository, PlatformTransactionManager transactionManager) {
 		return new StepBuilder("imprimeOlaMundoStep", jobRepository)
-				.tasklet((StepContribution contribution, ChunkContext chunkContext) -> {
-					System.out.println("Olá Mundo!");
-					return RepeatStatus.FINISHED;
-				}, transactionManager)
+				.tasklet(imprileOlaMundoTasklet(null), transactionManager)
 				.build();
+	}
+	
+	@Bean
+	@StepScope
+	public Tasklet imprileOlaMundoTasklet(@Value("#{jobParameters['name']}") String name) {
+		return (StepContribution contribution, ChunkContext chunkContext) -> {
+			System.out.println("Olá Mundo! " + name + "!");
+			return RepeatStatus.FINISHED;		
+		};
 	}
 }
